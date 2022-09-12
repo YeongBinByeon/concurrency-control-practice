6d662f42ef451e5631c0498151230228d9d0d7ee
yeongbin <yeongbin6483@gmail.com>
2022-09-13 05:39:34 +0900

    Database Lock 활용하는 부분까지 커밋

---
+++ stock/src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,63 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NamedLockStockFacadeTest {
+    @Autowired
+    private NamedLockStockFacade namedLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before(){
+        Stock stock = new Stock(1L, 100l);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after(){
+        stockRepository.deleteAll();
+    }
+
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i=0; i<threadCount; i++){
+            executorService.submit(()->{
+                try {
+                    // 아무 처리 안 된 상태에서 여러 쓰레드에서 동시에 재고 값 감소시키는 로직 수행
+                    namedLockStockFacade.decrease(1l, 1l);
+                }
+                finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1*100) = 0 기대
+        assertEquals(0,stock.getQuantity());
+    }
+}
